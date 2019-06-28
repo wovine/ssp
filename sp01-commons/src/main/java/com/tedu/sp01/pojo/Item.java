c0633341f150da6a05d8981ce86eb191ff0875f0
张帅 <1319197065@qq.com>
2019-06-28 10:23:10 +0800

    21111

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,16 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	//离开家安福科技阿尔法可见其表空间服务起步快放假哈口诀表 
+	//天
+	private Integer id;
+	private String name;
+	private Integer number;
+}
